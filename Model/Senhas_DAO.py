d877140d559b0833449ceaff810eda4c4069ad0f
Diogo Pereira Lima <87611476+DiogoWorkGit@users.noreply.github.com>
2022-09-04 23:50:43 -0300

    making password manager 1/3

--- Model/Senhas_DAO.py
+++ Model/Senhas_DAO.py
@@ -2,3 +2,35 @@ class managePasswords:
     def __init__(self):
         username = ""
         password = ""
+        nome = ""
+        url = ""
+    
+    def insertPassword (self, w, x, y, z):
+        global username
+        global password
+        global nome
+        global url
+        
+        username = w
+        password = x
+        nome = y
+        url = z
+        
+    def chagePassword (self, w, x, y, z):
+        global username
+        global password
+        global nome
+        global url
+        
+        username = w
+        password = x
+        nome = y
+        url = z
+        
+    def deletePassword (self):
+        global username
+        global password
+        global nome
+        global url
+        
+        
